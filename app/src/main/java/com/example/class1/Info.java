0b88533ad4c2a00517a88454b0255f84081a1425
sumeet3-0 <sumeetchavan124@gmail.com>
2020-04-09 15:05:32 +0530

    third commit

---
+++ app/src/main/java/com/example/class1/Info.java
@@ -0,0 +1,111 @@
+package com.example.class1;
+
+public class Info {
+    String name,school,parent,occParent,address,mobNo,email,password,board,medium,std;
+
+    public Info() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public String getOccParent() {
+        return occParent;
+    }
+
+    public void setOccParent(String occParent) {
+        this.occParent = occParent;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobNo() {
+        return mobNo;
+    }
+
+    public void setMobNo(String mobNo) {
+        this.mobNo = mobNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getStd() {
+        return std;
+    }
+
+    public void setStd(String std) {
+        this.std = std;
+    }
+
+    public Info(String name, String school, String parent, String occParent, String address, String mobNo, String email, String password, String board, String medium, String std) {
+        this.name = name;
+        this.school = school;
+        this.parent = parent;
+        this.occParent = occParent;
+        this.address = address;
+        this.mobNo = mobNo;
+        this.email = email;
+        this.password = password;
+        this.board = board;
+        this.medium = medium;
+        this.std = std;
+    }
+}
