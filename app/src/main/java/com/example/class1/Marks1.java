89a9b6181a74c1dbc47a3c24aa2840ce8e88369a
sumeet3-0 <sumeetchavan124@gmail.com>
2020-04-26 17:02:40 +0530

    26 april

---
+++ app/src/main/java/com/example/class1/Marks1.java
@@ -0,0 +1,14 @@
+package com.example.class1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Marks1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks1);
+    }
+}
