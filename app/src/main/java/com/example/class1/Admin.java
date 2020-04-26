20a1b0ccdb2ad1bd2cd61fd3eed55f5cdcd0c882
sumeet3-0 <sumeetchavan124@gmail.com>
2020-04-26 19:21:13 +0530

    26 april

--- app/src/main/java/com/example/class1/Admin.java
+++ app/src/main/java/com/example/class1/Admin.java
@@ -9,14 +9,15 @@ import androidx.cardview.widget.CardView;
 
 public class Admin extends AppCompatActivity {
 
-    CardView attendenceCV,marksCV,feesCV;
+    CardView attendenceCV,marksCV,feesCV,viewCV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         attendenceCV = findViewById(R.id.attendenceCV);
         marksCV = findViewById(R.id.marksCV);
-        feesCV = findViewById(R.id.fees);
+        feesCV = findViewById(R.id.feesCV);
+        viewCV = findViewById(R.id.viewCV);
         attendenceCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +36,13 @@ public class Admin extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(),AdminMarks1.class);
+                startActivity(i);
+            }
+        });
+        viewCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ViewList.class);
                 startActivity(i);
             }
         });
